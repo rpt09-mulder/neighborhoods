5dde72b273b46ca7947a6b28a01055c3f5cc70dc
Allen Hsieh <allenhsiehhr@gmail.com>
2018-11-28 21:20:19 -0800

    Completed schema for a room Seperated code that used to live in seed.js into it's own model folder

---
+++ database/models/room.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+let roomSchema = mongoose.Schema({
+  id: {type: Number, unique: true},
+  user: String,
+  title: String,
+  type: String,
+  city: String,
+  selfCheckin: Boolean,
+  superhost: Boolean,
+  descriptions: Array,
+  amenities: Array,
+  rooms: Array
+});
+
+const RoomModel = mongoose.model('Room', roomSchema);
+
+const findAll = (callback) => {
+  RoomModel.find({}, callback);
+};
+
+const findByID = (id, callback) => {
+  RoomModel.find({id: id}, callback);
+};
+
+const insertOne = (room, callback) => {
+  RoomModel.create(room, callback);
+};
+
+module.exports = {
+  findAll,
+  findByID,
+  insertOne
+};
