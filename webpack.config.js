f03a201de21e6d82af1c91e1919ddeefca501c1c
Allen Hsieh <allenhsiehhr@gmail.com>
2018-11-30 16:58:17 -0800

    Finally config'd webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,21 @@ const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.js`,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        quer: {
-          presets: ['react', 'env']
+        test: /\.jsx/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
         }
       }
     ]
