586d188d698af5a3df6cb8f7d6159f82161924a3
Allen Hsieh <allenhsiehhr@gmail.com>
2018-12-04 12:49:06 -0800

    Wrote a number of server unit tests using mock

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const app = require('./app.js');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rooms');
 
 let port = process.env.PORT || 3001;
 
