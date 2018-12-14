58f9c6780c4ec160b1ee665144a46786db2fbb6e
Allen Hsieh <allenhsiehhr@gmail.com>
2018-12-13 20:41:57 -0800

    Removed console logs

--- client/src/components/DetailsApp.jsx
+++ client/src/components/DetailsApp.jsx
@@ -10,10 +10,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    // console.log('window.location', window.location);
-    // console.log('window.location.pathname:', window.location.pathname);
     axios.get(`http://localhost:3001/details${window.location.pathname}`).then(res => {
-      // console.log('axios response', res.data.data[0]);
       this.setState({
         data: res.data.data[0]
       });
@@ -26,7 +23,6 @@ class App extends React.Component {
       <div>
         <h1>This is the Rooms component</h1>
         <div>
-          {console.log('dataaaa', this.state.data)}
           <h5>{type}</h5>
           <h3>{title}</h3>
           <h5>{city}</h5>
