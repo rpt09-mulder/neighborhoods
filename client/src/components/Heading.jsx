a31685e8049809062eee5da6b07389cff320c68e
Allen Hsieh <allenhsiehhr@gmail.com>
2018-12-30 12:34:15 -0800

    linted heading.jsx

--- client/src/components/Heading.jsx
+++ client/src/components/Heading.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Heading = ({type, title, city}) => (
+const Heading = ({ type, title, city }) => (
   <div>
     <div>{type}</div>
     <div>{title}</div>
     <div>{city}</div>
   </div>
 );
+
+Heading.propTypes = {
+  type: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired
+};
 
 export default Heading;
