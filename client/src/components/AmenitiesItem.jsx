782ad8c00a84c60cbc9620cfad65918403b5482e
Allen Hsieh <allenhsiehhr@gmail.com>
2018-12-30 16:02:03 -0800

    linted AmenitiesItem.jsx

--- client/src/components/AmenitiesItem.jsx
+++ client/src/components/AmenitiesItem.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const AmenitiesItem = ({amenity}) => (
+const AmenitiesItem = ({ amenity }) => (
   <div>
     {amenity}
   </div>
 );
+
+AmenitiesItem.propTypes = {
+  amenity: PropTypes.string.isRequired
+};
 
 export default AmenitiesItem;
