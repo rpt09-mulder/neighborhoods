3a0d2b9e24943949004e0edce25ee2805bddf938
Allen Hsieh <allenhsiehhr@gmail.com>
2019-01-02 18:38:12 -0800

    quick way of handling icons for amenities

--- client/src/components/AmenitiesItem.jsx
+++ client/src/components/AmenitiesItem.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils, faSuitcase, faParking, faWifi, faWarehouse, faLaptop } from '@fortawesome/free-solid-svg-icons';
 
-const AmenitiesItem = ({ amenity }) => (
+const AmenitiesItem = ({ amenity }) => {
+  const iconGuide = {
+    "Kitchen": faUtensils,
+    "Iron": faSuitcase,
+    "Free parking on premises": faParking,
+    "Wifi": faWifi,
+    "Hangers": faWarehouse,
+    "Laptop friendly workspace": faLaptop,
+  }
+
+  return (
   <div>
+    <FontAwesomeIcon icon={iconGuide[amenity]} />
     {amenity}
   </div>
-);
+  );
+};
 
 AmenitiesItem.propTypes = {
   amenity: PropTypes.string.isRequired,
